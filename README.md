0dbca1ae13dfcaf2df57e1c59534fa1bea824144
Bryan Duxbury <bryan@redplanetlabs.com>
2020-10-04 10:47:24 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# nippy-serializable-fns
+[![Clojars Project](https://img.shields.io/clojars/v/com.rpl/vector-backed-sorted-map.svg)](https://clojars.org/com.rpl/vector-backed-sorted-map)
+
+A sorted map implementation designed for ultra-fast initialization and merge operations. See [our blog post] (TODO) for more discussion of this project and its implementation.
+
+## Usage
+
+TODO
+
+## License
+
+Copyright © 2019 - 2020 Red Planet Labs Inc.
+
+Distributed under the Apache Software License version 2.0
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject com.rpl/vector-backed-sorted-map "0.1.0"
+  :description "A sorted map implementation designed for ultra-fast initialization and merge operations."
+  :url "https://github.com/redplanetlabs/vector-backed-sorted-map"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.taoensso/nippy "2.14.0"]]
+  :test-paths ["test"]
+  :plugins [[lein-exec "0.3.7"]]
+  :profiles {:dev {:dependencies
+                   [[org.clojure/test.check "0.9.0"]
+                    [org.clojure/spec.alpha "0.2.176"]]}
+             :check {:warn-on-reflection true}
+             :nrepl {:lein-tools-deps/config {:resolve-aliases [:nrepl]}}
+             :aot {:aot :all}
+             }
+  )
